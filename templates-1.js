52351a6df5458f87851de67ff1fb9b96a7b2a8f0
Célia Chazel <chazel.celia@gmail.com>
2018-02-25 23:06:47 +0100

    Add php request example

--- templates-1.js
+++ templates-1.js
@@ -186,3 +186,20 @@ function shortenUrl(input) {
         }
     });
 }
+
+// PHP REQUEST EXAMPLE
+function testRequest(input) {
+    var url = 'http://php/main.php';
+    var xhr = new XMLHttpRequest;
+    xhr.responseType = 'text';
+
+    xhr.onreadystatechange = function () {
+        if (xhr.readyState === XMLHttpRequest.DONE) {
+            console.log(xhr.response)
+        }
+    }
+    xhr.open('GET', './php/main.php?req=' + input)
+    xhr.send();
+}
+
+testRequest('OSS117');
